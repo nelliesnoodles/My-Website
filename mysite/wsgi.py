294140d68d0ff8b0eb2a1d6eb01a73f5d07e8e6f
Nellie Tobey <sarahssmiling2@gmail.com>
2018-11-14 07:52:44 -0800

    november 13

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,26 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+try:
+	path = '/home/NelliesNoodles/nelliesnoodles_mysite/mysite'
+	if path not in sys.path:
+	    sys.path.insert(0, path)
 
-application = get_wsgi_application()
+	os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+	application = get_wsgi_application()
+
+except:
+	# to update from my computer, home path is different than
+        #pythonanywhere path?
+	path = '/home/nellie/nelliesnoodles_mysite/mysite'
+	if path not in sys.path:
+	    sys.path.insert(0, path)
+
+	os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+	application = get_wsgi_application()
