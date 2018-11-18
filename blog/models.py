83e2337ec23862f3590f234e5e4bb9aa585f2065
Nellie Tobey <sarahssmiling2@gmail.com>
2018-11-18 17:41:18 +0000

    error with settings py file

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
     publish_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.now()
+        self.publish_date = timezone.now()
         self.save()
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
     return render(request, 'blog/under_construction.html', {})
+
+def blog_list(request):
+    #Use this on separate html link
+    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    return render(request, 'blog/blog_trial.html', {'posts':posts})
